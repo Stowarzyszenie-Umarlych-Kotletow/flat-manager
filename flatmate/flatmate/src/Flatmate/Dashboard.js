c76dffdf2cdc588dd654b4c384798e0246dba532
starwader <starwader@gmail.com>
2021-12-10 17:26:30 +0100

    Add calendar in the dashboard

--- flatmate/flatmate/src/Flatmate/Dashboard.js
+++ flatmate/flatmate/src/Flatmate/Dashboard.js
@@ -2,18 +2,35 @@ import * as React from "react";
 import styles from "../static/native_elements_styles";
 import {Text, View} from "react-native";
 import {Button} from "react-native-elements";
-
+import Calendar from 'react-calendar'
+import {useState} from "react";
+import 'react-calendar/dist/Calendar.css';
 
 export function DashboardScreen({navigation}) {
+
+    const [value, onChange] = useState(new Date());
+
+    function handleClickCalendar(){
+        console.log(value)
+    }
+
     return (
         <View style={styles.accScreenContainer}>
             <View style={styles.accFormView}>
 
                 <Text style={styles.logoText}>Dashboard</Text>
+                <div align="center">
+                    <Calendar
+                        onChange={onChange}
+                        value={value}
+                        onClickDay={handleClickCalendar}
+                    />
+                </div>
 
                 <Button
                     buttonStyle={styles.highButton}
                     title="test"
+
                     // onPress={() => navigation.navigate('ManageFlats')}
                 />
 
