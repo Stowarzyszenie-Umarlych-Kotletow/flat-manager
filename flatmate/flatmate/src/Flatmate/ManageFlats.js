6648c7870e7e69680ee45ffd22cbc9b72b2f1c48
starwader <starwader@gmail.com>
2021-12-10 17:41:54 +0100

    Add join/create popup windows

--- flatmate/flatmate/src/Flatmate/ManageFlats.js
+++ flatmate/flatmate/src/Flatmate/ManageFlats.js
@@ -2,9 +2,31 @@ import * as React from "react";
 import styles from "../static/native_elements_styles";
 import {Text, View} from "react-native";
 import {Button} from "react-native-elements";
-import {Row, Col} from 'react-bootstrap'
+import {Row, Col, Modal, InputGroup, FormControl, Button as BButton} from 'react-bootstrap'
+import {useState} from "react";
 
 export function ManageFlatsScreen({navigation, currentFlat = null, setCurrentFlat}) {
+    const [showJoinFlat, setShowJoinFlat] = useState(false);
+    const handleCloseJoinFlat = () => setShowJoinFlat(false);
+    const handleShowJoinFlat = () => setShowJoinFlat(true);
+
+    const [showCreateFlat, setShowCreateFlat] = useState(false);
+    const handleCloseCreateFlat = () => setShowCreateFlat(false);
+    const handleShowCreateFlat = () => setShowCreateFlat(true);
+
+    function handleJoinFlat(){
+        handleCloseJoinFlat()
+        setCurrentFlat(123)
+        navigation.popToTop()
+    }
+
+    function handleCreateFlat(){
+        handleCloseCreateFlat()
+        setCurrentFlat(123)
+        navigation.popToTop()
+        // navigation.navigate("DashboardScreen")
+    }
+
     function handleClickOnFlat(flat) {
         setCurrentFlat(flat)
         console.log(flat)
@@ -21,12 +43,14 @@ export function ManageFlatsScreen({navigation, currentFlat = null, setCurrentFla
                         <Button
                             buttonStyle={styles.bluButton}
                             title="Create flat"
+                            onPress={() => handleShowCreateFlat()}
                         />
                     </Col>
                     <Col>
                         <Button
                             buttonStyle={styles.bluButton}
                             title="Join flat"
+                            onPress={() => handleShowJoinFlat()}
                         />
                     </Col>
                 </Row>
@@ -62,6 +86,55 @@ export function ManageFlatsScreen({navigation, currentFlat = null, setCurrentFla
                     onPress={() => handleClickOnFlat(6)}
                 />
 
+                <Modal show={showJoinFlat} onHide={handleCloseJoinFlat}>
+
+                    <Modal.Header closeButton>
+                        <Modal.Title>Join an existing flat</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p className="text-sm-left">Flat code:</p>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                placeholder="6-character code"
+                                aria-label="flat-code"
+                                aria-describedby="basic-addon1"
+                            />
+                        </InputGroup>
+
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <BButton variant="secondary" onClick={handleCloseJoinFlat}>Cancel</BButton>
+                        <BButton variant="primary" onClick={handleJoinFlat}>Join</BButton>
+                    </Modal.Footer>
+
+                </Modal>
+
+                <Modal show={showCreateFlat} onHide={handleCloseCreateFlat}>
+
+                    <Modal.Header closeButton>
+                        <Modal.Title>Create a new flat</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p className="text-sm-left">Flat name:</p>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                placeholder="Name"
+                                aria-label="flat-code"
+                                aria-describedby="basic-addon1"
+                            />
+                        </InputGroup>
+
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <BButton variant="secondary" onClick={handleCloseCreateFlat}>Cancel</BButton>
+                        <BButton variant="primary" onClick={handleCreateFlat}>Create</BButton>
+                    </Modal.Footer>
+
+                </Modal>
             </View>
         </View>)
 }
