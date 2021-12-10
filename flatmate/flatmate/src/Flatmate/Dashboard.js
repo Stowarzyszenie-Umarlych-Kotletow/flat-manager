8a790385c23465192f90638444621637cc136ded
starwader <starwader@gmail.com>
2021-12-10 18:43:41 +0100

    Add AddUser popup window

--- flatmate/flatmate/src/Flatmate/Dashboard.js
+++ flatmate/flatmate/src/Flatmate/Dashboard.js
@@ -5,12 +5,27 @@ import {Button} from "react-native-elements";
 import Calendar from 'react-calendar'
 import {useState} from "react";
 import 'react-calendar/dist/Calendar.css';
+import {Button as BButton, FormControl, InputGroup, Modal} from "react-bootstrap";
+import Countdown, {zeroPad} from 'react-countdown';
+import clippy from '../static/clippy.svg'
 
 export function DashboardScreen({navigation}) {
+    const [showGenJoinCode, setShowGenJoinCode] = useState(false);
+    const handleCloseGenJoinCode = () => setShowGenJoinCode(false);
+    const handleShowGenJoinCode = () => setShowGenJoinCode(true);
 
     const [value, onChange] = useState(new Date());
 
-    function handleClickCalendar(){
+    const counterRenderer = ({minutes, seconds}) => (
+        <div align="center">
+            <span className="text-black">
+                Code valid for {zeroPad(minutes)}:{zeroPad(seconds)} min
+            </span>
+        </div>
+
+    );
+
+    function handleClickCalendar() {
         console.log(value)
     }
 
@@ -28,12 +43,40 @@ export function DashboardScreen({navigation}) {
                 </div>
 
                 <Button
-                    buttonStyle={styles.highButton}
-                    title="test"
-
-                    // onPress={() => navigation.navigate('ManageFlats')}
+                    buttonStyle={styles.bluButton}
+                    title="Add user to the flat"
+                    onPress={() => handleShowGenJoinCode()}
                 />
+                <Modal show={showGenJoinCode} onHide={handleCloseGenJoinCode}>
 
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add user</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p className="text-sm-left" align="center">Join code:</p>
+
+                        <div className="input-group mb-3 justify-content-md-center">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">ASDFAS</span>
+                            </div>
+                            <BButton className="btn-secondary" data-clipboard-target="ASDFAS">
+                                <img src={clippy} alt="Copy to clipboard" width="20px"/>
+                            </BButton>
+                        </div>
+                        <Countdown
+                            onComplete={handleCloseGenJoinCode}
+                            date={Date.now() + 5 * 60 * 1000}
+                            renderer={counterRenderer}
+                        />
+
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <BButton variant="secondary" onClick={handleCloseGenJoinCode}>Close</BButton>
+                    </Modal.Footer>
+
+                </Modal>
             </View>
         </View>)
 }
