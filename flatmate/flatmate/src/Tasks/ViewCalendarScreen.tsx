b47886a5fc0c5a005cb9c2132ae7be478b84f938
Filip Drygaś <tejotemku@gmail.com>
2022-01-05 13:51:21 +0100

    push

--- flatmate/flatmate/src/Tasks/ViewCalendarScreen.tsx
+++ flatmate/flatmate/src/Tasks/ViewCalendarScreen.tsx
@@ -8,29 +8,40 @@ import taskService from "../services/task.service";
 
 
 export function ViewCalendarScreen() {
-
+           
 
     const tasks = useAppSelector(state => state.flat.tasks);
     const selectedFlatId = useAppSelector(state => state.flat.selectedFlatId);
-    console.log(tasks);
 
-    const convertTasks = () => {
+  
+
+   async function convertTasks () {
+        var date = await taskService.getFlatSchedule(selectedFlatId, {from: new Date('December 17, 1995 03:24:00'), until: new Date('December 17, 2095 03:24:00')});
+     
         var events = [];
-        Object.values(tasks).map(task => {
-            var date = taskService.getFlatSchedule(selectedFlatId, {from: new Date('December 17, 1995 03:24:00'), until: new Date('December 17, 2095 03:24:00')});
+        var tast_vars = date.data.taskInstances;
+        Object.values(tast_vars).map(task_var => { 
+            Object.values(task_var).map(task => {
+
+            var name = "";
+            Object.values(tasks).map(tasker => {
+                if(tasker.id == task.id){ name = tasker.name}
+            })
             events.push({
-                title: task.name,
+                title: name,
                 _id: task.id,
-                _assignee: "-",
-                _deadline: new Date(2022, 0, 1),
-                start: new Date(2022, 0, 2),
-                end: new Date(2022, 0, 3),
+                _assignee: task.userId,
+                _deadline: task.date,
+                start: task.date,
+                end: task.date,
             }) 
+            }); 
+            
         });
         return events;
     };
 
-
+    console.log(convertTasks().then(eventer => {return eventer}));
     function eventClicked(event) {
         setTaskState(event)
         setShowTaskDetailsModal(true)
@@ -45,7 +56,7 @@ export function ViewCalendarScreen() {
             height: "calc(100vh - 75px)",
         }}>
             <Calendar
-                events={convertTasks()}
+                events={[]}
                 height={510}
                 mode={'month'}
                 showTime={true}
