091ae9e33ff3947bca09d04610bca63519c5f588
tedtheripper <lemon.juice0731@gmail.com>
2022-01-09 17:56:05 +0100

    fixed lombok

--- flatmanager/src/main/java/com/pis/flatmanager/dto/users/UserDto.java
+++ flatmanager/src/main/java/com/pis/flatmanager/dto/users/UserDto.java
@@ -1,10 +1,12 @@
 package com.pis.flatmanager.dto.users;
 
 import com.pis.flatmanager.model.UserFlat;
+import lombok.Builder;
 import lombok.Value;
 
 import java.util.List;
 
+@Builder
 @Value
 public class UserDto {
 
