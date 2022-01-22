863a2c897dc7f3ddd6aa5b07e2303fb0eb2c12fa
Filip Drygaś <tejotemku@gmail.com>
2022-01-22 01:10:40 +0100

    Logged Navigator Bugfix

--- flatmate/flatmate/src/LoggedNavigator.tsx
+++ flatmate/flatmate/src/LoggedNavigator.tsx
@@ -20,77 +20,79 @@ const userSettings = require("./static/userGear.svg") as string;
 const LoggedStack = createStackNavigator();
 
 export function LoggedNavigator({navigation}) {
-    const username = useAppSelector((state) => state.auth.user?.username);
-    
-    const {flatId, flat} = useFlat();
-    const dispatch = useAppDispatch();
+  const username = useAppSelector((state) => state.auth.user?.username);
+  
+  const {flatId, flat} = useFlat();
+  const dispatch = useAppDispatch();
 
 
-    return (<LoggedStack.Navigator
-        screenOptions={{
-            headerMode: 'screen',
-            headerTintColor: 'white',
-            headerStyle: {backgroundColor: 'black', height: 75},
-            headerRight: () => (
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('ManageScreen')}
-                    style={styles.columnView}
-                >
-                <Text style={{color: "white"}}> {username} </Text>
-                <img src={userSettings} alt="open settings" style={{width: '35px', height: '35px'}}/>
-            </TouchableOpacity>
+  return (
+  <LoggedStack.Navigator
+    screenOptions={{
+      headerMode: 'screen',
+      headerTintColor: 'white',
+      headerStyle: {backgroundColor: 'black', height: 75},
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate('ManageScreen')}
+          style={styles.columnView}
+        >
+        <Text style={{color: "white"}}> {username} </Text>
+        <img src={userSettings} alt="open settings" style={{width: '35px', height: '35px'}}/>
+      </TouchableOpacity>
 
-            ),
-            animationEnabled: true,
-        }}>
-        {flat == null ? (<LoggedStack.Screen name="FirstManageFlatsScreen" options={{
-            title: 'Manage flats'
-        }}>
-            {props => <ManageFlatsScreen {...props}/>}
-        </LoggedStack.Screen>) : (<>
-            <LoggedStack.Screen name="DashboardScreen" options={{
-                title: 'Dashboard'
-            }}>
-                {props => <DashboardScreen {...props}/>}
-            </LoggedStack.Screen>
+      ),
+      animationEnabled: true,
+  }}
+	>
+    {flat == null ? (<LoggedStack.Screen name="FirstManageFlatsScreen" options={{
+      title: 'Manage flats'
+    }}>
+      {props => <ManageFlatsScreen {...props}/>}
+    </LoggedStack.Screen>) : (<>
+      <LoggedStack.Screen name="DashboardScreen" options={{
+        title: 'Dashboard'
+      }}>
+        {props => <DashboardScreen {...props}/>}
+      </LoggedStack.Screen>
 
-            <LoggedStack.Screen name="ManageFlatsScreen" options={{
-                title: 'Manage flats'
-            }}>
-                {props => <ManageFlatsScreen {...props}/>}
-            </LoggedStack.Screen>
+      <LoggedStack.Screen name="ManageFlatsScreen" options={{
+        title: 'Manage flats'
+      }}>
+        {props => <ManageFlatsScreen {...props}/>}
+      </LoggedStack.Screen>
 
-            <LoggedStack.Screen name="ViewCalendarScreen" options={{
-                title: 'Calendar'
-            }}>
-                {() => <ViewCalendarScreen />}
-            </LoggedStack.Screen>
-            <LoggedStack.Screen name="TransactionManagementView" options={{
-                title: 'Transaction Management'
-            }}>
-                {() => <TransactionManagementView />}
-            </LoggedStack.Screen>
-            <LoggedStack.Screen name="Tasks" options={{
-                title: 'Tasks'
-            }}>
-                {props => <ManageTasks {...props} />}
-            </LoggedStack.Screen>
-            <LoggedStack.Screen name="Users" options={{
-                title: 'Users'
-            }}>
-                {props => <ManageUsers {...props} />}
-            </LoggedStack.Screen>
-        </>)}
+      <LoggedStack.Screen name="ViewCalendarScreen" options={{
+        title: 'Calendar'
+      }}>
+        {() => <ViewCalendarScreen />}
+      </LoggedStack.Screen>
+      <LoggedStack.Screen name="TransactionManagementView" options={{
+        title: 'Transaction Management'
+      }}>
+        {(props) => <TransactionManagementView {...props} />}
+      </LoggedStack.Screen>
+      <LoggedStack.Screen name="Tasks" options={{
+        title: 'Tasks'
+      }}>
+        {props => <ManageTasks {...props} />}
+      </LoggedStack.Screen>
+      <LoggedStack.Screen name="Users" options={{
+        title: 'Users'
+      }}>
+        {props => <ManageUsers {...props} />}
+      </LoggedStack.Screen>
+    </>)}
 
 
-        <LoggedStack.Screen name="ManageScreen" options={{
-            title: 'Manage account'
-        }}>
-            {props => <ManageScreenModal {...props}/>}
-        </LoggedStack.Screen>
+    <LoggedStack.Screen name="ManageScreen" options={{
+      title: 'Manage account'
+    }}>
+      {props => <ManageScreenModal {...props}/>}
+    </LoggedStack.Screen>
 
-        <LoggedStack.Screen name="ChangePasswordScreen" component={ChangePasswordScreen} options={{
-            title: 'Change Password'
-        }}/>
-    </LoggedStack.Navigator>)
+    <LoggedStack.Screen name="ChangePasswordScreen" component={ChangePasswordScreen} options={{
+      title: 'Change Password'
+    }}/>
+  </LoggedStack.Navigator>)
 }
