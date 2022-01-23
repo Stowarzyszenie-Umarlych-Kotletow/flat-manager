81fd8622a935278924d8efeab1eb1874ee7c6ef3
tedtheripper <lemon.juice0731@gmail.com>
2022-01-23 14:59:16 +0100

    added Transaction groups gto

--- flatmanager/src/main/java/com/pis/flatmanager/service/TransactionServiceImpl.java
+++ flatmanager/src/main/java/com/pis/flatmanager/service/TransactionServiceImpl.java
@@ -13,7 +13,7 @@ import com.pis.flatmanager.service.interfaces.UserService;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.pis.flatmanager.dto.transactions.TransactionGroupDto;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -32,11 +32,11 @@ public class TransactionServiceImpl implements TransactionService {
     private FlatService flatService;
 
     @Override
-    public TransactionGroup createTransactionGroup(User user, CreateTransactionGroupDto dto) throws AccessForbiddenException {
+    public TransactionGroupDto createTransactionGroup(User user, CreateTransactionGroupDto dto) throws AccessForbiddenException {
         flatService.getFlatAsUser(user, dto.getFlatId());
         var transactions = dto.getTransactions().stream().map(transaction -> new Transaction(transaction.getName(), transaction.getPrice())).collect(Collectors.toList());
         var transactionGroup = new TransactionGroup(dto.getName(), user.getId(), dto.getFlatId(), transactions, dto.getUsersConnected());
-        return transactionRepository.save(transactionGroup);
+        return transactionRepository.save(transactionGroup).asDto();
     }
 
     @Override
@@ -46,21 +46,18 @@ public class TransactionServiceImpl implements TransactionService {
     }
 
     @Override
-    public TransactionGroup getTransactionGroup(User user, UUID transactionGroupId) throws AccessForbiddenException {
+    public TransactionGroupDto getTransactionGroup(User user, UUID transactionGroupId) throws AccessForbiddenException {
         var transactionGroup = transactionRepository.findById(transactionGroupId)
                 .orElseThrow(() -> new EntityNotFoundException(String.format("TransactionGroup %s does not exist", transactionGroupId)));
         flatService.getFlatAsUser(user, transactionGroup.getFlatId());
-        return transactionGroup;
+        return transactionGroup.asDto();
     }
 
     @Override
-    public List<TransactionGroup> getTransactionGroupsByFlatId(User user, UUID flatId) throws AccessForbiddenException {
+    public List<TransactionGroupDto> getTransactionGroupsByFlatId(User user, UUID flatId) throws AccessForbiddenException {
         flatService.getFlatAsUser(user, flatId);
         var transactionGroups = transactionRepository.findTransactionGroupsByFlatId(flatId);
-        if(transactionGroups.isEmpty()) {
-            throw new EntityNotFoundException(String.format("TransactionGroup with flatId %s does not exist", flatId));
-        }
-        return transactionGroups;
+        return transactionGroups.stream().map(TransactionGroup::asDto).collect(Collectors.toList());
     }
 
 }
