aa5e9698f4d7b00e781c22a47843f5d5cb85004b
Przemysław Rozwałka <przem2003@o2.pl>
2022-01-24 16:41:02 +0100

    Finishing touches

--- flatmanager/src/main/java/com/pis/flatmanager/service/TransactionServiceImpl.java
+++ flatmanager/src/main/java/com/pis/flatmanager/service/TransactionServiceImpl.java
@@ -52,6 +52,7 @@ public class TransactionServiceImpl implements TransactionService {
         for(int i = 0; i < 100; i++) {
             var sum = debtMap.values().stream().map(BigDecimal::abs).reduce(BigDecimal.ZERO, BigDecimal::add);
             if (sum.equals(previousSum) || sum.floatValue() < 0.1f) {
+                // break condition -> the result is good enough
                 break;
             }
             previousSum = sum;
