9c7df062f2e0a3c3200ef0546610f0acafa843fb
Przemysław Rozwałka <przem2003@o2.pl>
2022-01-24 21:38:17 +0100

    Calendar tweaks

--- flatmate/flatmate/src/screens/tasks/ViewCalendarScreen.tsx
+++ flatmate/flatmate/src/screens/tasks/ViewCalendarScreen.tsx
@@ -1,25 +1,32 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text} from 'react-native';
-import {Button} from 'react-native-elements';
-import {Calendar} from 'react-native-big-calendar';
-import {TaskDetailsModal} from "../../components/tasks/TaskDetailsModal";
-import {useAppDispatch} from "../../store";
-import {flatApi, useGetFlatScheduleQuery} from '../../features/api/flat-api';
-import {useFlat} from '../../features/hooks';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { Calendar, HasDateRange } from 'react-native-big-calendar';
+import { TaskDetailsModal } from "../../components/tasks/TaskDetailsModal";
+import { useAppDispatch } from "../../store";
+import { flatApi, useGetFlatScheduleQuery } from '../../features/api/flat-api';
+import { useFlat } from '../../features/hooks';
 import TaskEvent from '../../models/event.model';
-import {scheduleToEvents, taskInstanceToFrontendState} from '../../helpers/task-helper';
+import { scheduleToEvents, taskInstanceToFrontendState } from '../../helpers/task-helper';
 import styles from "../../static/styles";
-import {TaskFrontendState} from "../../models/task.model";
+import { TaskFrontendState } from "../../models/task.model";
 import format from 'date-fns/format';
+import { default as useStateRef } from 'react-usestateref'
 
+
+type DateContainer = {
+    wasSet: boolean;
+    date: Date;
+};
 
 export function ViewCalendarScreen() {
     const query = { from: new Date('December 17, 1995 03:24:00').toISOString(), until: new Date('December 17, 2095 03:24:00').toISOString() };
 
     const { flatId, flatTasks } = useFlat();
     const dispatch = useAppDispatch();
-    const { isLoading, currentData: taskSchedule} = useGetFlatScheduleQuery({ flatId, data: query }, {refetchOnMountOrArgChange: true});
-    const [currentDateStr, setCurrentDateStr] = useState("");
+    const { isLoading, currentData: taskSchedule } = useGetFlatScheduleQuery({ flatId, data: query }, { refetchOnMountOrArgChange: true });
+    const [currentDate, setCurrentDate] = useState<DateContainer>({ date: new Date(), wasSet: false });
+
 
     useEffect(() => {
         dispatch(flatApi.util.invalidateTags(['flatTasks']));
@@ -38,17 +45,32 @@ export function ViewCalendarScreen() {
         return scheduleToEvents(taskSchedule?.taskInstances, getTaskName);
     }
 
+    function handleDateChanged(range: HasDateRange) {
+        const { date, wasSet } = currentDate;
+        if ((+range[0] != +date) !== wasSet) {
+            // XOR condition: make sure there is no callback loop
+            setCurrentDate({date: range[0], wasSet: false});
+        }
+    }
+
+    function navigateMonth(offset: number) {
+        const { date } = currentDate;
+        const newDate = new Date(+date);
+        newDate.setMonth(date.getMonth() + offset);
+        setCurrentDate({ date: newDate, wasSet: true });
+    }
+
     function eventClicked(event) {
-        setActiveTaskInstance({instance: event.instance, taskId: event.taskId});
+        setActiveTaskInstance({ instance: event.instance, taskId: event.taskId });
         setShowTaskDetailsModal(true)
     }
 
 
     function doRenderEvent(event: TaskEvent, props) {
         let style = {}
-        let taskFrontendState:TaskFrontendState = taskInstanceToFrontendState(event.instance)
+        let taskFrontendState: TaskFrontendState = taskInstanceToFrontendState(event.instance)
 
-        switch(taskFrontendState){
+        switch (taskFrontendState) {
             case TaskFrontendState.COMPLETED:
                 style = styles.eventCompleted;
                 break;
@@ -66,23 +88,28 @@ export function ViewCalendarScreen() {
         return <Button
             buttonStyle={style}
             title={event.title}
-            titleStyle={{ fontWeight: '700', fontSize: 11}}
+            titleStyle={{ fontWeight: '700', fontSize: 11 }}
             onPress={() => eventClicked(event)}
         />
     }
 
     const [showTaskDetailsModal, setShowTaskDetailsModal] = useState(false);
-    const [activeTaskInstance, setActiveTaskInstance] = useState({instance: null, taskId: null});
+    const [activeTaskInstance, setActiveTaskInstance] = useState({ instance: null, taskId: null });
 
     return (
         <View style={{
             overflow: "scroll",
             height: "calc(100vh - 75px)",
         }}>
-            <Text style={styles.bigTextCenter}>{currentDateStr}</Text>
+            <View style={[styles.viewRow, { justifyContent: "space-between" }]}>
+                <Button title={"<--"} onPress={() => navigateMonth(-1)} />
+                <Text style={styles.bigTextCenter}>{format(currentDate.date, "MMMM yyyy")}</Text>
+                <Button title={"-->"} onPress={() => navigateMonth(1)} />
+            </View>
             <Calendar
                 events={getCalendar()}
-                onChangeDate={(date) => setCurrentDateStr(format(date[0], "MMMM yyyy"))}
+                onChangeDate={handleDateChanged}
+                date={currentDate.date}
                 height={510}
                 mode={'month'}
                 showTime={false}
